1080df8e683951fb422085ea6c54ef849f96a42d
Alok Tripathy <kullalok@gmail.com>
2021-02-16 20:30:18 -0800

    Integrated 1.5D partitioning

--- examples/pytorch/graphsage/distributed_code/train_full_15d.py
+++ examples/pytorch/graphsage/distributed_code/train_full_15d.py
@@ -5,6 +5,7 @@ Code: https://github.com/williamleif/graphsage-simple
 Simple reference implementation of GraphSAGE.
 """
 import argparse
+import math
 import os
 import time
 import numpy as np
@@ -20,7 +21,7 @@ from dgl.data import register_data_args, load_data
 from sageconv import SAGEConvAgg, SAGEConvMLP
 
 
-def broad_func( self, graph, ampbyp, agg, inputs):
+def broad_func(self, graph, ampbyp, agg, inputs):
   node_count = graph.number_of_nodes()
 
   n_per_proc = math.ceil(float(node_count) / (size / replication))
@@ -137,11 +138,114 @@ class GraphSAGE(nn.Module):
     # output layer
     self.mlp_layers.append(SAGEConvMLP(n_hidden, n_classes, aggregator_type)) # activation None
 
-  def forward(self, graph, inputs):
+  def forward(self, graph, inputs, ampbyp):
     h = self.dropout(inputs)
     for l, (agg_layer, mlp_layer) in enumerate(zip(self.agg_layers, self.mlp_layers)):
       h = GraphSAGEFn.apply(self, graph, ampbyp, agg_layer, mlp_layer, h)
     return h
+
+# Normalize all elements according to KW's normalization rule
+def scale_elements(adj_matrix, adj_part, node_count, row_vtx, col_vtx, normalization):
+    if not normalization:
+        return adj_part
+
+    adj_part = adj_part.coalesce()
+    deg = torch.histc(adj_matrix[0].double(), bins=node_count)
+    deg = deg.pow(-0.5)
+
+    row_len = adj_part.size(0)
+    col_len = adj_part.size(1)
+
+    dleft = torch.sparse_coo_tensor([np.arange(0, row_len).tolist(),
+                                     np.arange(0, row_len).tolist()],
+                                     deg[row_vtx:(row_vtx + row_len)].float(),
+                                     size=(row_len, row_len),
+                                     requires_grad=False, device=torch.device("cpu"))
+
+    dright = torch.sparse_coo_tensor([np.arange(0, col_len).tolist(),
+                                     np.arange(0, col_len).tolist()],
+                                     deg[col_vtx:(col_vtx + col_len)].float(),
+                                     size=(col_len, col_len),
+                                     requires_grad=False, device=torch.device("cpu"))
+    # adj_part = torch.sparse.mm(torch.sparse.mm(dleft, adj_part), dright)
+    ad_ind, ad_val = torch_sparse.spspmm(adj_part._indices(), adj_part._values(), 
+                                            dright._indices(), dright._values(),
+                                            adj_part.size(0), adj_part.size(1), dright.size(1))
+
+    adj_part_ind, adj_part_val = torch_sparse.spspmm(dleft._indices(), dleft._values(), 
+                                                        ad_ind, ad_val,
+                                                        dleft.size(0), dleft.size(1), adj_part.size(1))
+
+    adj_part = torch.sparse_coo_tensor(adj_part_ind, adj_part_val, 
+                                                size=(adj_part.size(0), adj_part.size(1)),
+                                                requires_grad=False, device=torch.device("cpu"))
+
+    return adj_part
+
+# Split a COO into partitions of size n_per_proc
+# Basically torch.split but for Sparse Tensors since pytorch doesn't support that.
+def split_coo(adj_matrix, node_count, n_per_proc, dim):
+    vtx_indices = list(range(0, node_count, n_per_proc))
+    vtx_indices.append(node_count)
+
+    am_partitions = []
+    for i in range(len(vtx_indices) - 1):
+        am_part = adj_matrix[:,(adj_matrix[dim,:] >= vtx_indices[i]).nonzero().squeeze(1)]
+        am_part = am_part[:,(am_part[dim,:] < vtx_indices[i + 1]).nonzero().squeeze(1)]
+        am_part[dim] -= vtx_indices[i]
+        am_partitions.append(am_part)
+
+    return am_partitions, vtx_indices
+
+def oned_partition(rank, size, replication, inputs, adj_matrix, normalization):
+    node_count = inputs.size(0)
+    n_per_proc = math.ceil(float(node_count) / (size / replication))
+
+    am_partitions = None
+    am_pbyp = None
+
+    rank_c = rank // replication
+    # Compute the adj_matrix and inputs partitions for this process
+    with torch.no_grad():
+        # Column partitions
+        am_partitions, vtx_indices = split_coo(adj_matrix, node_count, n_per_proc, 1)
+
+        proc_node_count = vtx_indices[rank_c + 1] - vtx_indices[rank_c]
+        am_pbyp, _ = split_coo(am_partitions[rank_c], node_count, n_per_proc, 0)
+        for i in range(len(am_pbyp)):
+            if i == size // replication - 1:
+                last_node_count = vtx_indices[i + 1] - vtx_indices[i]
+                am_pbyp[i] = torch.sparse_coo_tensor(am_pbyp[i], torch.ones(am_pbyp[i].size(1)), 
+                                                        size=(last_node_count, proc_node_count),
+                                                        requires_grad=False)
+
+                am_pbyp[i] = scale_elements(adj_matrix, am_pbyp[i], node_count, vtx_indices[i], 
+                                                vtx_indices[rank_c], normalization)
+            else:
+                am_pbyp[i] = torch.sparse_coo_tensor(am_pbyp[i], torch.ones(am_pbyp[i].size(1)), 
+                                                        size=(n_per_proc, proc_node_count),
+                                                        requires_grad=False)
+
+                am_pbyp[i] = scale_elements(adj_matrix, am_pbyp[i], node_count, vtx_indices[i], 
+                                                vtx_indices[rank_c], normalization)
+
+        for i in range(len(am_partitions)):
+            proc_node_count = vtx_indices[i + 1] - vtx_indices[i]
+            am_partitions[i] = torch.sparse_coo_tensor(am_partitions[i], 
+                                                    torch.ones(am_partitions[i].size(1)), 
+                                                    size=(node_count, proc_node_count), 
+                                                    requires_grad=False)
+            am_partitions[i] = scale_elements(adj_matrix, am_partitions[i], node_count, 0, 
+                                                vtx_indices[i], normalization)
+
+        input_partitions = torch.split(inputs, math.ceil(float(inputs.size(0)) / (size / replication)), dim=0)
+
+        adj_matrix_loc = am_partitions[rank_c]
+        inputs_loc = input_partitions[rank_c]
+
+    print(f"rank: {rank} adj_matrix_loc.size: {adj_matrix_loc.size()}", flush=True)
+    print(f"rank: {rank} inputs_loc.size: {inputs_loc.size()}", flush=True)
+    return inputs_loc, adj_matrix_loc, am_pbyp
 
 
 def evaluate(model, graph, features, labels, nid):
@@ -216,8 +320,19 @@ def main(args):
     group = dist.new_group(list(range(size)))
     row_groups, col_groups = get_proc_groups(rank, size, args.replication)
 
+    # Partition input graph and features
+    edges = g.all_edges() # Convert DGLGraph to COO for partitioning
+    edges = torch.stack([edges[0], edges[1]], dim=0)
+    features_loc, g_loc, ampbyp = oned_partition(rank, size, args.replication, features, edges, 
+                                                        args.normalization)
+
+    # Convert COO back to DGLGraph
+    # Uses hardcoded types, doesn't include all the metadata in original g
+    g_loc = dgl.heterograph({("_N", "_N", "_E"): (g_loc._indices()[0], g_loc._indices()[1])}) 
+    for i in range(len(ampbyp)):
+        ampbyp[i] = dgl.heterograph({("_N", "_N", "_E"): (ampbyp[i]._indices()[0], ampbyp[i]._indices()[1])})
+
     # create GraphSAGE model
-    # def __init__(self, in_feats, n_hidden, n_classes, n_layers, activation, dropout, aggregator_type, rank, size, replication, group, row_groups, col_groups):
     model = GraphSAGE(in_feats,
                       args.n_hidden,
                       n_classes,
@@ -245,7 +360,7 @@ def main(args):
         if epoch >= 3:
             t0 = time.time()
         # forward
-        logits = model(g, features)
+        logits = model(g, features, ampbyp)
         loss = F.cross_entropy(logits[train_nid], labels[train_nid])
 
         optimizer.zero_grad()
@@ -286,6 +401,8 @@ if __name__ == '__main__':
                         help="Aggregator type: mean/gcn/pool/lstm")
     parser.add_argument("--replication", type=int, default=1,
                         help="replciation factor for 1.5D algorithm")
+    parser.add_argument("--normalization", type=bool, default=False,
+                        help="normalize adjacency matrix for training")
     args = parser.parse_args()
     print(args)
 
